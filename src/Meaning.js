ccb63d2628704ec7469b395baed5abbabe95e08d
Samantha-a-T <115659673+Samantha-a-T@users.noreply.github.com>
2023-01-24 14:27:22 -0700

    add Synonym component, pass synonym value through respective component and loop.

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms.js";
 
 export default function Meaning(props) {
   return (
@@ -8,10 +9,12 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Defintion:</strong>
               {definition.definition}
               <br />
-
+              <strong>Example:</strong>
               <em>{definition.example}</em>
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
