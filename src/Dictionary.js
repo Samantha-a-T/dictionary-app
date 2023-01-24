55cbeff332e7cf6936c4a8a25464434f574254ed
Samantha-a-T <115659673+Samantha-a-T@users.noreply.github.com>
2023-01-24 13:52:23 -0700

    add Meaning and Results components. Add loops for definitions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results.js";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
+
   function handleResponse(response) {
-    console.log(response);
+    setResults(response.data[0]);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -21,6 +24,8 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+   
+      <Results results={results} />
     </div>
   );
 }
