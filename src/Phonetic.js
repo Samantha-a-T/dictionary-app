93d440848fc17444a04544255dd083dd017b170a
Samantha-a-T <115659673+Samantha-a-T@users.noreply.github.com>
2023-01-26 19:22:17 -0700

    Add styling to phonetic and results

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "./Phonetic.css"
+import "./Phonetic.css";
 
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <h2>{props.phonetic.text}</h2>
+      <h2 className="phonetic-text">{props.phonetic.text}</h2>
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen here
       </a>
