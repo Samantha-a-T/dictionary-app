3b71b355a0226447b462ff033f868798914ddfdf
Samantha-a-T <115659673+Samantha-a-T@users.noreply.github.com>
2023-01-29 17:18:35 -0700

    add float to results sections

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -6,10 +6,9 @@ export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       <h2 className="phonetic-text"> {props.phonetic.text}</h2>
-      <div className="audio-player"> <ReactAudioPlayer src={props.phonetic.audio} autoPlay controls /> </div>
-     
-    
-     
+      <div className="audio-player">
+        <ReactAudioPlayer src={props.phonetic.audio} autoPlay controls />{" "}
+      </div>
     </div>
   );
 }
